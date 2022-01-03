ded11696157d0b3cce152c38d91c5cf162eebacd
johan <johan52754@gmail.com>
2022-01-03 15:31:57 -0500

    Primer commit

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv=require("dotenv-webpack");
 
 module.exports = {
     mode: 'production', // LE INDICO EL MODO EXPLICITAMENTE
@@ -68,6 +69,7 @@ module.exports = {
                     to: "assets/images",
                 }
             ]
-        })
+        }),
+        new Dotenv()
     ]
 }
